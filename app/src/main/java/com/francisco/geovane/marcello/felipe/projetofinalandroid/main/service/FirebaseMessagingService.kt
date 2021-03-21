1dbee0c33788ae119fa09db52994b71c95cb9c4c
marcellochuahy <marcello@chuahy.org>
2021-03-21 15:51:18 -0300

    Perform two cloud messages (places, restaurants)

---
+++ app/src/main/java/com/francisco/geovane/marcello/felipe/projetofinalandroid/main/service/FirebaseMessagingService.kt
@@ -0,0 +1,9 @@
+package com.francisco.geovane.marcello.felipe.projetofinalandroid.main.service
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class FirebaseMessagingService: FirebaseMessagingService() {
+
+
+
+}
